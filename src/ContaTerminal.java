8e916d7d4baf7c940d55cf0a8e66617413b0bf00
ArlessonMoura <arlessonmss@gmail.com>
2024-08-01 20:55:11 -0300

    One more project to improve my java skills

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,17 +1,228 @@
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public void openAccount() {
-        Scanner write = new Scanner(System.in);
+    private String clientName;
+    private int accountNumber;
+    private int bankBranchId;
+    private int bankOperation;
+    private double balanceValue;
+    private final Scanner write = new Scanner(System.in);
 
-        System.out.println("""
+    public String getClientName() {
+        return clientName;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getBankBranchId() {
+        return bankBranchId;
+    }
+
+    public int getBankOperation() {
+        return bankOperation;
+    }
+
+    public double getBalanceValue() {
+        return balanceValue;
+    }
+
+    public void setClientName() {
+        boolean nameVerify = false;
+
+        while (!nameVerify) {
+            System.out.println("""
                ||====================================================================||
-                                       PRESS ENTER TO CONTINUE
+                                        What is your name?
                ||====================================================================||
                 """);
-        write.nextLine();
-        write.close();
 
+            clientName = write.nextLine();
+            if (clientName != null && !clientName.isBlank()) {
+
+                System.out.println("""
+               ||====================================================================||
+                                %s
+                                          beautiful name!
+               ||====================================================================||
+                """.formatted(clientName));
+                nameVerify = true;
+            } else {
+                System.out.println("""
+               ||====================================================================||
+                                   You need provide a Valid entry.
+               ||====================================================================||
+                                                ||
+                """);
+            }
+
+        }
 
     }
+
+    public void setAccountNumber() {
+        boolean accountVerify = false;
+
+
+        while (!accountVerify) {
+            System.out.println("""
+               ||====================================================================||
+                                  Choose your account number ID
+                                           4 numbers.
+               ||====================================================================||
+                """);
+
+            try {
+                String userEntry = write.nextLine().trim();
+
+                if (userEntry.matches("\\d{4}")) {
+                    accountNumber = Integer.parseInt(userEntry);
+                    System.out.println("""
+                       ||====================================================================||
+                                                   You choose: %d
+                       ||====================================================================||
+                        """.formatted(accountNumber));
+                    accountVerify = true;
+                } else {
+                    System.out.println("""
+                       ||====================================================================||
+                              Invalid input! Please enter exactly 4 numeric digits only.
+                       ||====================================================================||
+                                                        ||
+                        """);
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("""
+                   ||====================================================================||
+                               Invalid input! Please enter numeric values only.
+                   ||====================================================================||
+                                                    ||
+                    """);
+            }
+
+        }
+    }
+
+    public void setBankBranchId() {
+        Scanner write = new Scanner(System.in);
+        boolean bankBranchVerify = false;
+
+        while (!bankBranchVerify) {
+            System.out.println("""
+               ||====================================================================||
+                                  Choose your bank branch number ID
+                                           3 numbers.
+               ||====================================================================||
+                """);
+
+            try {
+                String userEntry = write.nextLine().trim();
+
+                if (userEntry.matches("\\d{3}")) {
+                    bankBranchId = Integer.parseInt(userEntry);
+                    bankBranchVerify = true;
+                } else {
+                    System.out.println("""
+                       ||====================================================================||
+                              Invalid input! Please enter exactly 3 numeric digits only.
+                       ||====================================================================||
+                                                        ||
+                        """);
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("""
+                   ||====================================================================||
+                               Invalid input! Please enter numeric values only.
+                   ||====================================================================||
+                                                    ||
+                    """);
+            }
+
+        }
+    }
+
+    public void setOperationBank() {
+        Scanner write = new Scanner(System.in);
+        boolean bankOperationVerify = false;
+
+        while (!bankOperationVerify) {
+            System.out.println("""
+               ||====================================================================||
+                                  Choose your operation bank number ID
+                                         Just only 1 numbers.
+               ||====================================================================||
+                """);
+
+            try {
+                String userEntry = write.nextLine().trim();
+
+                if (userEntry.matches("\\d")) {
+                    bankOperation = Integer.parseInt(userEntry);
+                    bankOperationVerify = true;
+                } else {
+                    System.out.println("""
+                       ||====================================================================||
+                              Invalid input! Please enter exactly 1 numeric digits only.
+                       ||====================================================================||
+                                                        ||
+                        """);
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("""
+                   ||====================================================================||
+                               Invalid input! Please enter numeric values only.
+                   ||====================================================================||
+                                                    ||
+                    """);
+            }
+
+        }
+
+    }
+
+    public void setBalanceValue() {
+        Scanner write = new Scanner(System.in);
+        boolean balanceVerify = false;
+
+        while (!balanceVerify) {
+            System.out.println("""
+                ||====================================================================||
+                                   Choose your bank balance value
+                             (This is a peculiar bank, don't you think?)
+                ||====================================================================||
+            """);
+
+            try {
+                balanceValue = write.nextDouble();
+
+                System.out.println("""
+                    ||====================================================================||
+                                           Is It your best?
+                                Is that the greatest number that you can think?
+                                            So much humble.
+                    ||====================================================================||
+                """);
+
+                balanceVerify = true;
+
+            } catch (Exception e) {
+                System.out.println("""
+                    ||====================================================================||
+                            Invalid input! Please enter a numeric value (e.g., 123.45).
+                    ||====================================================================||
+                                                    ||
+                """);
+                write.nextLine();
+            }
+        }
+
+        write.close();
+    }
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner write = new Scanner(System.in);
+
         System.out.println("""
                                      _ _.-'`-._ _                \s
                                     ;.'___$$___'.;               \s
@@ -13,11 +17,38 @@ public class Main {
                         |LI LI LI LI||LI||LI||LI||LI LI LI LI|   \s
                      ,,;;,;;;,;;;,;;;,;;;,;;;,;;;,;;,;;;,;;;,;;,,\s
                     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                
+                                
                   Greetings, I am Oracle, your personal banking assistant.
                 I'm here to assist you every step to create your bank account.
                 """);
+
+        System.out.println("""
+                ||====================================================================||
+                                        PRESS ENTER TO CONTINUE
+                ||====================================================================||
+                 """);
+        write.nextLine();
+
         ContaTerminal terminal = new ContaTerminal();
-        terminal.openAccount();
+
+        terminal.setClientName();
+        terminal.setAccountNumber();
+        terminal.setBankBranchId();
+        terminal.setOperationBank();
+        terminal.setBalanceValue();
+
+
+        System.out.println("""
+                    ||====================================================================||
+                                           Account Summary
+                    ||====================================================================||
+                           Name: %s
+                           Account Number: %d
+                           Branch ID: %d-%d
+                           Balance: %.2f
+                    ||====================================================================||
+                """.formatted(terminal.getClientName(), terminal.getAccountNumber(), terminal.getBankBranchId(),
+                terminal.getBankOperation(), terminal.getBalanceValue()));
+
     }
 }
